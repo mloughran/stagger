2198e00e85502edc788823f5d7d7a4d9c3dc9683
Martyn Loughran <me@mloughran.com>
2013-03-19 20:37:52 +0000

    Separated out the zmq pair code out of client

--- goserver/client.go
+++ goserver/client.go
@@ -2,31 +2,20 @@ package main
 
 import "log"
 
-import zmq "github.com/pebbe/zmq3"
-
 func RunClient(info ClientRef) {
-	sock, _ := zmq.NewSocket(zmq.PAIR)
-	defer sock.Close()
-	log.Print("Connecting to ", info.Address)
-	sock.Connect(info.Address)
+	events := NewZmqClient(info.Address)
 
-	on_message := make(chan string)
-	go func() {
-		var s string
-		for {
-			// TODO: Handle error
-			s, _ = sock.Recv(0)
-			on_message <- s
-		}
-	}()
-	
 	for {
 		select {
-		case <- info.Mailbox:
-			log.Print("Requesting stats from ", info.Address)
-			sock.Send("Stats please!", zmq.DONTWAIT)
-		case msg := <- on_message:
+		case <-info.Mailbox:
+			log.Print("[client] Requesting stats from ", info.Address)
+			events.SendMessage <- "Stats please!"
+		case msg := <-events.OnMessage:
+			log.Print("[client] Received stats")
 			log.Print(msg)
+		case <-events.OnClose:
+			log.Print("[client] Connection to ", info.Address, " closed")
+			return
 		}
 	}
 }
