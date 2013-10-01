mloughran/stagger

commit 314faa609e9628c49dd8dea89f59eb824a51a59d

Clean up pair Conn code & handle some more errors
Moving all the cases that could cause a close into the main goroutine removed the rather brittle 2-way close signalling.

If any error is raised, close the client immediately

pair/conn.go
@@ -5,11 +5,11 @@ import (
 )
 
 type Conn struct {
-	OnMethod    chan zmqMessage
-	OnClose     chan (bool)
-	sendMessage chan zmqMessage
-	pair        *zmq.Socket
-	addr        string
+	OnMethod      chan zmqMessage
+	OnClose       chan bool
+	sendMessage   chan zmqMessage
+	addr          string
+	shouldConnect bool
 }
 
 type zmqMessage struct {
@@ -17,112 +17,104 @@ type zmqMessage struct {
 	Params []byte
 }
 
-// NewConn creates a Conn(ection) which includes callback channels on which you
-// must select.
+// NewConn creates a Connection. You must select on OnMethod and OnClose
 func NewConn() *Conn {
-	sock, _ := zmq.NewSocket(zmq.PAIR)
+	return &Conn{make(chan zmqMessage), make(chan bool), make(chan zmqMessage), "", false}
+}
+
+// ShouldConnect notifies the Connection that it should Connect when Run called
+func (c *Conn) ShouldConnect(addr string) {
+	c.addr = addr
+	c.shouldConnect = true
+}
+
+// Send sends a message to the Connection
+func (c *Conn) Send(method string, params []byte) {
+	c.sendMessage <- zmqMessage{method, params}
+}
+
+func (c *Conn) Run() {
+	recvMessage := make(chan ([][]byte))
+	shouldClose := false
+
+	pair, _ := zmq.NewSocket(zmq.PAIR)
 
 	// Set a HWM to force an error when sending, thereby detecting that the
 	// client has gone away
-	if err := sock.SetSndhwm(1); err != nil {
-		info.Printf("[pair] Error setting HWM: %v", err)
+	if err := pair.SetSndhwm(1); err != nil {
+		info.Printf("[pair] Error in SetSndhwm: %v", err)
+		return
 	}
 
-	return &Conn{make(chan zmqMessage), make(chan bool), make(chan zmqMessage), sock, ""}
-}
+	// Timeout reads after 61s so that this goroutine can close
+	if err := pair.SetRcvtimeo(9e9); err != nil {
+		info.Printf("[pair] Error in SetRcvtimeo: %v", err)
+		return
+	}
 
-// Connnect the underlying pair connection to the given address
-func (z *Conn) Connect(addr string) {
-	z.addr = addr
-	z.pair.Connect(addr)
-}
+	if c.shouldConnect {
+		if err := pair.Connect(c.addr); err != nil {
+			info.Printf("[pair] Error connecting to client: %v", err)
+			return
+		}
+	}
 
-// Send a message
-func (z *Conn) Send(method string, params []byte) {
-	z.sendMessage <- zmqMessage{method, params}
-}
-
-// Run starts reading on the pair connection, and sending messages.
-// Requires a background goroutine for reading.
-func (z *Conn) Run() {
-	sock := z.pair
-
-	// Removing because it causes crash... (due to read goroutine)
-	// defer sock.Close()
-
-	// These are used so that we can stop the reading goroutine - hacky...
-	sock.SetRcvtimeo(61e9) // 61s
-	closed := false
-
-	// Used to signal shutdown to the main goroutine
-	sig_shutdown := make(chan bool)
-
-	// TODO: This isn't correct because sock not threadsafe...
+	// Goroutine handles reading from zmq
 	go func() {
-		defer func() {
-			debug.Printf("[zmqclient] Closing socket")
-			sock.Close()
-		}()
-
 		for {
-			parts, err := sock.RecvMessageBytes(0)
+			parts, err := pair.RecvMessageBytes(0)
 
-			if closed {
-				// Returns from this (zmq reading) goroutine
+			if shouldClose {
+				debug.Printf("[pair] Closing pair socket")
+				pair.Close()
 				return
 			}
 
-			if err != nil {
-				debug.Printf("[zmqclient] Recv err: %v", err)
-			} else {
-				s := string(parts[0])
-
-				// Non-prefixed ping & ping are deprecated. TODO: remove
-				if s == "ping" || s == "pair:ping" {
-					debug.Print("[zmqclient] Received ping, sending pong")
-					z.Send("pair:pong", []byte(""))
-				} else if s == "pong" || s == "pair:pong" {
-					debug.Print("[zmqclient] Received pong")
-					// Do nothing
-				} else if s == "pair:shutdown" {
-					debug.Print("[zmqclient] Received shutdown")
-					sig_shutdown <- true
-				} else {
-					if len(parts) != 2 {
-						info.Printf("[zmqclient] Unepected message %v with %v parts", s, len(parts))
-					} else {
-						debug.Printf("[zmqclient] Received message %s", s)
-						z.OnMethod <- zmqMessage{s, parts[1]}
-					}
-				}
+			if err == nil {
+				recvMessage <- parts
 			}
 		}
 	}()
 
 	defer func() {
-		closed = true
-		z.OnClose <- true
+		shouldClose = true
+		c.OnClose <- true
 	}()
 
 	for {
 		select {
-		case <-sig_shutdown:
-			return
-		case msg := <-z.sendMessage:
-			// Note: I considered using sock.SendMessage but unfortunately that
-			// doesn't support arbitrary flags, and it basically does this anway.
-
-			// Set DONTWAIT so that Send doesn't block when HWM is reached
-
-			// In case of an error and return, an OnClose will be sent by defer
-
-			if _, err := sock.Send(msg.Method, zmq.DONTWAIT|zmq.SNDMORE); err != nil {
-				info.Printf("[zmqclient] Closing %v after err: %v", z.addr, err)
+		case msg := <-c.sendMessage:
+			// Note: I considered using the cleaner pair.SendMessage but
+			// unfortunately that doesn't currently support arbitrary flags
+			// Using DONTWAIT to avoid Send blocking when HWM is reached
+			if _, err := pair.Send(msg.Method, zmq.DONTWAIT|zmq.SNDMORE); err != nil {
+				info.Printf("[pair] Closing %v after err: %v", c.addr, err)
 				return
 			}
-			if _, err := sock.SendBytes(msg.Params, zmq.DONTWAIT); err != nil {
-				info.Printf("[zmqclient] Closing %v after err: %v", z.addr, err)
+			if _, err := pair.SendBytes(msg.Params, zmq.DONTWAIT); err != nil {
+				info.Printf("[pair] Closing %v after err: %v", c.addr, err)
 				return
+			}
+		case parts := <-recvMessage:
+			s := string(parts[0])
+
+			// Non-prefixed ping & ping are deprecated. TODO: remove
+			if s == "ping" || s == "pair:ping" {
+				debug.Print("[pair] Received ping, sending pong")
+				c.Send("pair:pong", []byte(""))
+			} else if s == "pong" || s == "pair:pong" {
+				debug.Print("[pair] Received pong")
+				// Do nothing
+			} else if s == "pair:shutdown" {
+				debug.Print("[pair] Received shutdown")
+				return
+			} else {
+				if len(parts) != 2 {
+					info.Printf("[pair] Unexpected message %v with %v parts", s, len(parts))
+				} else {
+					debug.Printf("[pair] Received message %s", s)
+					c.OnMethod <- zmqMessage{s, parts[1]}
+				}
 			}
 		}
 	}

pair/server.go
@@ -41,9 +41,8 @@ func (self *Server) Run() {
 	for {
 		select {
 		case addr := <-registration.Registrations:
-			debug.Print("Connecting to ", addr)
 			pc := NewConn()
-			pc.Connect(addr) // TODO: Error checking
+			pc.ShouldConnect(addr)
 			go pc.Run()
 
 			idIncr += 1

commit d231cae2cf2d0c8c56d20e54988b50c77b553506

Fixed another deadlock caused by use of Send
Caused by 314faa609e9628c49dd8dea89f59eb824a51a59d

pair/conn.go
@@ -81,26 +81,34 @@ func (c *Conn) Run() {
 		c.OnClose <- true
 	}()
 
+	// send method for use by this goroutine
+	send := func(method string, params []byte) (err error) {
+		// Note: I considered using the cleaner pair.SendMessage but
+		// unfortunately that doesn't currently support arbitrary flags
+		// Using DONTWAIT to avoid Send blocking when HWM is reached
+		if _, err = pair.Send(method, zmq.DONTWAIT|zmq.SNDMORE); err != nil {
+			return
+		}
+		if _, err = pair.SendBytes(params, zmq.DONTWAIT); err != nil {
+			return
+		}
+		return
+	}
+
 	for {
 		select {
 		case msg := <-c.sendMessage:
-			// Note: I considered using the cleaner pair.SendMessage but
-			// unfortunately that doesn't currently support arbitrary flags
-			// Using DONTWAIT to avoid Send blocking when HWM is reached
-			if _, err := pair.Send(msg.Method, zmq.DONTWAIT|zmq.SNDMORE); err != nil {
+			if err := send(msg.Method, msg.Params); err != nil {
 				info.Printf("[pair] Closing %v after err: %v", c.addr, err)
 				return
 			}
-			if _, err := pair.SendBytes(msg.Params, zmq.DONTWAIT); err != nil {
-				info.Printf("[pair] Closing %v after err: %v", c.addr, err)
-				return
-			}
+
 		case parts := <-recvMessage:
 			s := string(parts[0])
 
 			if s == "pair:ping" {
 				debug.Print("[pair] Received ping, sending pong")
-				c.Send("pair:pong", []byte(""))
+				send("pair:pong", []byte(""))
 			} else if s == "pair:pong" {
 				debug.Print("[pair] Received pong")
 				// Do nothing