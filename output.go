9fb58777dd5402f43200e829129d47b8ea1135d0
zimbatm <zimbatm@zimbatm.com>
2015-09-14 12:40:48 +0100

    Remove ZeroMQ

--- output.go
+++ output.go
@@ -1,10 +1,7 @@
-// Output receives aggregated data from the aggregator and exposes it to downstream stagger processes, to any other process which wishes to subscribe to data via ZMQ, and to all configured outputters
+// Output receives aggregated data from the aggregator and exposes it to
+// all configured outputters.
 
 package main
-
-import (
-	zmq "github.com/pebbe/zmq3"
-)
 
 type OutputStat struct {
 	Timestamp int64
@@ -24,24 +21,8 @@ func NewOutput() *Output {
 }
 
 func (o *Output) Run(complete_chan <-chan (*TimestampedStats)) {
-	pub, _ := zmq.NewSocket(zmq.PUB)
-
-	pub.Bind("tcp://*:5563")
-
+	// Forward all stats to outputters
 	for stats := range complete_chan {
-		// ZMQ pubsub
-		// TODO: Extract as outputter
-		for key, value := range stats.Dists {
-			output_stat := OutputStat{stats.Timestamp, value}
-
-			if b, err := marshal(output_stat); err == nil {
-				pub.Send(key.String(), zmq.SNDMORE) // Use stat name as channel?
-				pub.SendBytes(b, 0)
-			} else {
-				info.Printf("Error encoding as msgpack: %v", output_stat)
-			}
-		}
-
 		for _, op := range o.outputs {
 			op.Send(stats)
 		}
