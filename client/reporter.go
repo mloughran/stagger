d704774c0de69254a80ad27cf2af45f71a6e0cee
Michael Walker <mike@barrucadu.co.uk>
2016-07-13 16:13:56 +0100

    Add 'Reporter's, to report metrics without the name

---
+++ client/reporter.go
@@ -0,0 +1,66 @@
+package client
+
+import "github.com/pusher/stagger/conn"
+
+// A Reporter sends metrics to the stagger client.
+type Reporter interface {
+	// The name of the mertic this reports for.
+	Metric() conn.StatKey
+
+	// Report a value.
+	Report(float64)
+}
+
+// A CountReporter tracks the current value of a counter.
+type CountReporter struct {
+	client *Client
+	metric conn.StatKey
+}
+
+func (c *Client) NewCountReporter(metric conn.StatKey) CountReporter {
+	return CountReporter{c, metric}
+}
+
+func (r *CountReporter) Metric() conn.StatKey {
+	return r.metric
+}
+
+func (r *CountReporter) Report(v float64) {
+	r.client.ReportCount(r.metric, v)
+}
+
+// A CumulativeCountReporter tracks the current value of a cumulative counter.
+type CumulativeCountReporter struct {
+	client *Client
+	metric conn.StatKey
+}
+
+func (c *Client) NewCumulativeCountReporter(metric conn.StatKey) CumulativeCountReporter {
+	return CumulativeCountReporter{c, metric}
+}
+
+func (r *CumulativeCountReporter) Metric() conn.StatKey {
+	return r.metric
+}
+
+func (r *CumulativeCountReporter) Report(v float64) {
+	r.client.ReportCumulativeCount(r.metric, v)
+}
+
+// A DistributionReporter tracks the current value of a distribution.
+type DistributionReporter struct {
+	client *Client
+	metric conn.StatKey
+}
+
+func (c *Client) NewDistributionReporter(metric conn.StatKey) DistributionReporter {
+	return DistributionReporter{c, metric}
+}
+
+func (r *DistributionReporter) Metric() conn.StatKey {
+	return r.metric
+}
+
+func (r *DistributionReporter) Report(v float64) {
+	r.client.ReportDistribution(r.metric, v)
+}
